313b3226aa7bc44bb94c6683a10e61f35d1171fe
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-21 21:24:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,6 +53,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+    welcome
+  hand=initial_round
+  until hand>21 do
+    hand=hit?(hand)
+    display_card_total(hand)
+  end
+  end_game(hand)
 end
     

475d245756ecf436539c833668f0570624a98d6e
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-21 21:14:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,8 +37,8 @@ def hit?(card_total)
   user_input = get_user_input
   if user_input == "h"
        card_total += deal_card
-  elsif input == "s"
-   current_total
+  elsif user_input == "s"
+   card_total
  else
    invalid_command
   end
